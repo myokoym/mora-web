757abce79ff04c3971b27dc29dc29be4228bddd3
Masafumi Yokoyama <myokoym@gmail.com>
2012-10-08 03:53:41 +0900

    Startup memository

---
+++ config.ru
@@ -0,0 +1,4 @@
+require_relative 'memo_docks-local'
+
+run Sinatra::Application
+
---
+++ memository.rb
@@ -0,0 +1,38 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'grit'
+
+get "/" do
+  haml :index
+end
+
+post "/write" do
+  pass = params['key']
+  text = params['memo']
+  @message = write_memo(pass, text)
+  haml :index
+end
+
+get "/write/:text" do |param|
+  pass, *text = param.split(/:/)
+  text = text.join(":")
+  write_memo(pass, text)
+end
+
+private
+def write_memo(pass, text)
+  memo_file = ".docks/memo"
+  key = File.open(".key").read.chomp
+  if pass == key
+    File.open(memo_file, "a") {|f|
+      f.puts text
+    }
+    repository = Grit::Repo.new(".git")
+    repository.add(memo_file)
+    repository.commit_index("write memo.")
+    "done."
+  else
+    "Your not me..."
+  end
+end
